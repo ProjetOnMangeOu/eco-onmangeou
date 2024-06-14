d99c584174c44f4499cb94d3b6b7b6967f1b52b0
Sygix <17769298+Sygix@users.noreply.github.com>
2024-06-14 17:05:20 +0200

    first push

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
+import { ErrorPage } from './pages/ErrorPage'
+import Layout from './components/Layout'
+import Session from './pages/Session'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/:sessionId",
+        element: <Session />,
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
