955e27168beb159ece7b49efcc5ec9fa44593170
Sygix <17769298+Sygix@users.noreply.github.com>
2024-06-17 16:29:10 +0200

    style: fix logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import SessionMenu from "../components/Session/SessionMenu";
+import Logo from "../assets/logo.svg?react";
 
 const Home = () => {
   return (
     <div className="layout max-w-screen-lg flex flex-auto flex-col gap-12 items-center justify-center px-4 py-12 relative">
       
-      <img src="/src/assets/logo.svg" alt="Logo onmangeou" className=" translate-x-[3%] w-2/4 sm:max-w-xs" />
+      {/* <img src="/src/assets/logo.svg" alt="Logo onmangeou" className="translate-x-[3%] w-2/4 sm:max-w-xs" /> */}
+      <Logo className="translate-x-[3%] w-2/4 h-fit sm:max-w-xs" />
       
       <span className="flex flex-col gap-3">
         <h1 className="h0 text-center">Découvrez votre prochain coup de 💜 culinaire</h1>
