2dc7011209c0a3af4f7da7ab316e7ef5235e672d
Sygix <17769298+Sygix@users.noreply.github.com>
2024-06-17 16:31:58 +0200

    style: update logo in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,12 @@
 import { Link, Outlet } from "react-router-dom";
+import Logo from "../assets/logo.svg?react";
 
 const Layout = () => {
   return (
     <>
       <header className="sticky top-0 z-40 flex justify-center p-4">
         <nav className="flex gap-2 border-neutral-400 p-2 rounded-xl bg-neutral-950/60 backdrop-blur-lg text-white">
-          <img src="/src/assets/logo.svg" alt="Ynov" className="translate-x-[3%] w-12" />
+          <Logo className="translate-x-[3%] w-12 h-fit" />
           <ul className="flex justify-center">
             <li className="flex justify-center items-center rounded-full px-4 py-2 transition-colors duration-300 hover:bg-neutral-900/40">
               {/* @ts-expect-error passing unknown prop to a html element */}
