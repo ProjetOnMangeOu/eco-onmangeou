30d998254c6fe1789ec8124efb5b9b9520a73969
Sygix <17769298+Sygix@users.noreply.github.com>
2024-06-25 08:44:17 +0200

    style: fix on safari

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,12 +5,11 @@ const Layout = () => {
   return (
     <>
       <header className="sticky top-0 z-40 flex justify-center p-4">
-        <nav className="h-fit flex gap-2 border-neutral-400 p-2 rounded-xl bg-neutral-950/60 backdrop-blur-lg text-white">
-          <Logo className="translate-x-[3%] w-12 h-fit" />
+        <nav className="flex gap-2 border-neutral-400 p-2 rounded-xl bg-neutral-950/60 backdrop-blur-lg text-white">
+          <Logo className="translate-x-[3%] w-12 h-fit max-h-12" />
           <ul className="flex justify-center">
             <li className="flex justify-center items-center rounded-full px-4 py-2 transition-colors duration-300 hover:bg-neutral-900/40">
-              {/* @ts-expect-error passing unknown prop to a html element */}
-              <Link to="/" className="" name="test" >Accueil</Link>
+              <Link to="/" className="" title="Accueil" >Accueil</Link>
             </li>
           </ul>
         </nav>
